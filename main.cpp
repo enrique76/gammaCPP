18a078d204e32512e24ad0f38dcbb783bbde0c0f
Luis Enrique Cruz Acosta <kikecruz01@gmail.com>
2022-09-08 20:18:09 -0700

    variable - Se agrego operadores logicos con valor y variable ademar de evaluador

--- main.cpp
+++ main.cpp
@@ -2,20 +2,23 @@
 #include<conio.h>
 #include<stdlib.h>
 #include<windows.h>
-#include "termino.hpp"
-#include "operaciones.hpp"
-#include "ecuacion.hpp"
+//#include "termino.hpp"
+//#include "operaciones.hpp"
+//#include "ecuacion.hpp"
 #include"variable.hpp"
 
 using namespace std;
 
 int main(){
 
-	Variable *v = new Variable("x");
+	Variable *v = new Variable(2,"x",2);
+	
 
-	v->evaluar(6);
+	cout<<"f("<<v->getVariable()<<") = "; v->Imprimir(); 
 
-	cout<<v->getVariable()<<" = "<<v->getValor();
+	for(int n=0;n<10;n++){
+		cout<<"\nf("<<n<<") = "<<v->evaluar(n);
+	}
 
 	return 0;
 }
---
+++ variable.hpp
@@ -0,0 +1,363 @@
+#ifndef VARIABLE
+#define VARIABLE
+
+#include<iostream>
+#include<string>
+#include<math.h>
+
+using namespace std; // 
+
+class Variable{
+	public:
+		double coeficiente;
+		string variable;
+		double exponente;
+		double valor = NULL;
+		int pos = 0;
+	public:
+
+		// operadores 
+		Variable(string v){
+			this->variable = v;
+			this->exponente = 1;
+			this->coeficiente = 1;
+		}
+
+		Variable(double coeficiente,string variable,double exponente,int pos){
+			this->coeficiente = coeficiente;
+			this->variable = variable;
+			this->exponente = exponente;
+			this->pos = pos;
+		}
+
+		Variable(double coeficiente,string variable,double exponente){
+			this->coeficiente = coeficiente;
+			this->variable = variable;
+			this->exponente = exponente;
+		}
+
+		Variable(){
+			this->coeficiente = 0;
+			this->variable = "x";
+			this->exponente = 1;
+			this->pos = 0;
+		}
+
+		Variable(Variable *t,int i){
+			this->coeficiente = t->coeficiente;
+			this->variable = t->variable;
+			this->exponente = t->exponente;
+			this->pos = i;
+		}
+
+		Variable(Variable *t){
+			if(t->getValor() != NULL){
+				this->valor = t->getValor();
+				this->exponente = 1;
+				this->coeficiente = 1;
+				this->variable = t->getVariable();
+				this->pos = t->getPos();
+			}
+			else{
+				this->coeficiente = t->coeficiente;
+				this->variable = t->variable;
+				this->exponente = t->exponente;
+				this->pos = t->pos;
+			}
+		}
+
+		// destructor 
+
+		~Variable(){
+
+		}
+
+		// imprimir 
+
+		void Imprimir(){
+			if(this->coeficiente != 0){
+				if(this->coeficiente > 0){
+					if(this->pos != 0){
+						cout<<" + "<<this->coeficiente;
+					}
+					else{
+						cout<<this->coeficiente;
+					}
+				}
+				else{
+					cout<<" "<<this->coeficiente;
+				}
+
+				if(this->exponente == 0){
+					// no se imprime nada
+				}
+				else if(this->exponente == 1){
+					cout<<this->variable;
+				}
+				else{
+					cout<<this->variable<<"^"<<this->exponente;
+				}
+			}
+			else if(this->valor != NULL){
+				cout<<this->valor;
+			}
+		}
+
+		void Imprimir_full(){
+			if(this->coeficiente != 0){
+				if(this->coeficiente > 0){
+					if(this->pos != 0){
+						cout<<" + "<<this->coeficiente;
+					}
+					else{
+						cout<<this->coeficiente;
+					}
+				}
+				else{
+					cout<<" "<<this->coeficiente;
+				}
+
+				if(this->exponente == 0){
+					// no se imprime nada
+				}
+				else if(this->exponente == 1){
+					cout<<this->variable;
+				}
+				else{
+					cout<<this->variable<<"^"<<this->exponente;
+				}
+			}
+			else if(this->valor != NULL){
+				cout<<this->valor;
+			}
+
+			cout<<", "<<this->pos;
+		}
+
+		// setters 
+
+		void setVariable(string v){
+			this->variable = v;
+		}
+
+		void setCoeficiente(double c){
+			this->coeficiente = c;
+		}
+
+		void setExponente(double e){
+			this->exponente = e;
+		}
+
+		void setValor(double v){
+			this->valor = v;
+			this->exponente = 1;
+			this->coeficiente = 1;
+		}
+
+		void setPos(int v){
+			this->pos = v;
+		}
+		// getters 
+
+		string getVariable(){
+			return this->variable;
+		}
+
+		double getCoeficiente(){
+			return this->coeficiente;
+		}
+
+		double getExponente(){
+			return this->exponente;
+		}
+
+		double getValor(){
+			return this->valor;
+		}
+		
+		int getPos(){
+			return this->pos;
+		}
+		// operadores logicos por Variable =,==,!=,<,>,<=,>=
+
+		void operator = (Variable *t){
+			this->coeficiente = t->coeficiente;
+			this->variable = t->variable;
+			this->exponente = t->exponente;
+			this->pos = t->pos;
+			this->valor = t->valor;
+		}
+
+		bool operator == (Variable *t){
+			bool v = false; 
+
+			if((this->valor && t->getValor()) != NULL){ 
+				if(this->valor == t->getValor()){
+					v = true;
+				}
+			}
+			else if((this->coeficiente == t->coeficiente) && 
+				(this->variable == t->variable) && 
+				(this->exponente == t->exponente)){
+				v = true;
+			}
+
+			return v;
+		}
+
+		bool operator != (Variable *t){
+			bool v = false; 
+
+			if((this->valor && t->getValor()) != NULL){ 
+				if(this->valor != t->getValor()){
+					v = true;
+				}
+			}
+			else if((this->coeficiente != t->coeficiente) && 
+				(this->variable != t->variable) && 
+				(this->exponente != t->exponente)){
+				v = true;
+			}
+
+			return v;
+		}
+
+		bool operator < (Variable *t){
+			bool v = false; 
+
+			if((this->valor && t->getValor()) != NULL){ 
+				if(this->valor < t->getValor()){
+					v = true;
+				}
+			}
+			return v;
+		}
+		
+		bool operator > (Variable *t){
+			bool v = false; 
+
+			if((this->valor && t->getValor()) != NULL){ 
+				if(this->valor > t->getValor()){
+					v = true;
+				}
+			}
+			return v;
+		}
+		
+		bool operator <= (Variable *t){
+			bool v = false; 
+
+			if((this->valor && t->getValor()) != NULL){ 
+				if(this->valor <= t->getValor()){
+					v = true;
+				}
+			}
+			return v;
+		}
+		
+		bool operator >= (Variable *t){
+			bool v = false; 
+
+			if((this->valor && t->getValor()) != NULL){ 
+				if(this->valor >= t->getValor()){
+					v = true;
+				}
+			}
+			return v;
+		}
+
+		// operadores logicos por Valor  =,==,!=,<,>,<=,>=
+
+		void operator = (double v){
+			this->setValor(v);
+		}
+
+		bool operator == (double t){
+			bool v = false;
+
+			if(this->valor != NULL){
+				if(this->valor == t){
+					v = true;
+				}
+			}
+			
+			return v;
+		}
+
+		bool operator != (double t){
+			bool v = false;
+
+			if(this->valor != NULL){
+				if(this->valor != t){
+					v = true;
+				}
+			}
+			
+			return v;
+		}
+		
+		bool operator < (double t){
+			bool v = false;
+
+			if(this->valor != NULL){
+				if(this->valor < t){
+					v = true;
+				}
+			}
+			
+			return v;
+		}
+		
+		bool operator > (double t){
+			bool v = false;
+
+			if(this->valor != NULL){
+				if(this->valor > t){
+					v = true;
+				}
+			}
+			
+			return v;
+		}
+		
+		bool operator <= (double t){
+			bool v = false;
+
+			if(this->valor != NULL){
+				if(this->valor <= t){
+					v = true;
+				}
+			}
+			
+			return v;
+		}
+
+		bool operator >= (double t){
+			bool v = false;
+
+			if(this->valor != NULL){
+				if(this->valor >= t){
+					v = true;
+				}
+			}
+			
+			return v;
+		}		
+		
+		// operadores aridmeticos por Variable +,-,*,/,^,**,%
+		
+
+		// operadores aridmeticos por valor +,-,*,/,^,**,%
+		
+		
+
+	// evaluar   
+
+	double evaluar(double v){
+		return (this->coeficiente * pow(v,this->exponente));
+	}
+
+};
+
+#endif // Variable
+
